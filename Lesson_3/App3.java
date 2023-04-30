a10b7b79b790e31ba3ba8234570e86aba9b16cc6
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-04-30 22:26:10 +0300

    добавлен конспект 3 лекции

---
+++ Lesson_3/App3.java
@@ -0,0 +1,19 @@
+package Lesson_3;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App3 {
+    public static void main(String[] args) {
+        try (FileReader reader = new FileReader("test.md");
+                FileWriter writer = new FileWriter("test2.md")) {
+            while (reader.ready()) {
+                writer.write(reader.read());
+            }
+        } catch (RuntimeException | IOException e) {
+            System.out.println("Catch exception: " + e.getClass().getSimpleName());
+        }
+        System.out.println("Copy complete");
+    }
+}
