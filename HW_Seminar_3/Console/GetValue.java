b7e9cb69d246295e498f49c94d2c4674e24eb747
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-05-12 11:40:08 +0300

    создан запрос от пользователя

---
+++ HW_Seminar_3/Console/GetValue.java
@@ -0,0 +1,18 @@
+package HW_Seminar_3.Console;
+
+import java.util.Scanner;
+
+public class GetValue implements IGetValue {
+
+    Scanner in = new Scanner(System.in);
+
+    @Override
+    /**
+     * Метод запроса ввода от пользователя
+     */
+    public String getValue() {
+        System.out.print(" --> ");
+        return in.nextLine();
+    }
+    
+}
--- HW_Seminar_3/Console/IGetValue.java
+++ HW_Seminar_3/Console/IGetValue.java
@@ -4,5 +4,5 @@ package HW_Seminar_3.Console;
  * Интерфейс, который содержит метод запроса от пользователя
  */
 public interface IGetValue {
-    public String getValue(String msg);
+    public String getValue();
 }
