3b4cbcdf92671de82aff43aa57e37a6c37b2e1ff
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-05-12 11:48:02 +0300

    создан вывод пользователю

--- HW_Seminar_3/Console/ISetValue.java
+++ HW_Seminar_3/Console/ISetValue.java
@@ -1,5 +1,5 @@
 package HW_Seminar_3.Console;
 
 public interface ISetValue {
-    public String setValueString(String msg);
+    public void setValueString(String msg);
 }
---
+++ HW_Seminar_3/Console/SetValue.java
@@ -0,0 +1,10 @@
+package HW_Seminar_3.Console;
+
+public class SetValue implements ISetValue {
+
+    @Override
+    public void setValueString(String msg) {
+        System.out.println(msg);
+    }
+    
+}
